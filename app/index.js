2214b8252d8c6b72f95841e991f8f65a080dbce1
blackranja <josphatranja243@gmail.com>
2023-09-26 18:39:43 +0300

    Framework Update

--- app/index.js
+++ app/index.js
@@ -1,16 +1,30 @@
 import { View, SafeAreaView,Text } from 'react-native'
 import React from 'react'
-import { Stack } from 'expo-router'
-import Home from '../components/screens/Home'
-const index = () => {
+import { Stack, useRouter } from 'expo-router'
+import Products from '../components/screens/Products';
+//import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import { NavigationContainer} from '@react-navigation/native';
+import Details from '../components/screens/Details';
+import { ScrollView } from 'react-native-gesture-handler';
+import Welcome from '../components/Welcome';
+//const Stack=createNativeStackNavigator();
+
+const Home = () => {
+    const router=useRouter();
   return (
-    <SafeAreaView>
-        
-    <View>
-        <Home/>
-    </View>
+    <SafeAreaView style={{flex:1,backGroundColor:'#ffccff'}}>
+        <Stack.Screen options={{headerTitle:"TestOne"}}/>
+        <ScrollView>
+            <View>
+                <Welcome/>
+                <Products/>
+                <Details/>
+            </View>
+        </ScrollView>
     </SafeAreaView>
+   
+   
   )
 }
 
-export default index
+export default Home
---
+++ components/screens/Products.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Navbar from '../Navbar'
+import Footer from '../Footer'
+
+const Products = () => {
+  return (
+    <View>
+      <Navbar/>
+      <Footer/>
+      
+    </View>
+  )
+}
+
+export default Products
