3f6ab9e937ec286dc2d7d407bc95b45a9ebf8516
blackranja <josphatranja243@gmail.com>
2023-09-29 09:40:59 +0300

    Global Import/Export

---
+++ components/index.js
@@ -0,0 +1,10 @@
+import HomeHeader from './HomeHeader';
+import FocusedStatusBar from './FocusedStatusBar';
+import NFTCard from './cards/NFTCard';
+import {CircleButton,RectButton} from './Button';
+import DetailsDesc from './DetailDesc';
+import DetailsBid from './DetailsBid';
+
+export {
+    HomeHeader,NFTCard,FocusedStatusBar,CircleButton,RectButton,DetailsDesc,DetailsBid
+}
