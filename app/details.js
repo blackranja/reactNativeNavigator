1c8fb47d8fc2c02ac7f7c8c0b7f4f1e5920aabbb
blackranja <josphatranja243@gmail.com>
2023-09-29 11:15:09 +0300

    Local Import/Export

--- app/details.js
+++ app/details.js
@@ -1,45 +1,19 @@
-import { View, Text,SafeAreaView,ScrollView } from 'react-native'
-import { Stack, useRouter } from 'expo-router';
-import React from 'react'
-import {useFonts} from 'expo-font';
-import { DefaultTheme } from '@react-navigation/native';
+import { View, Text,SafeAreaView,Image,StatusBar,Flatlist } from 'react-native';
+import {COLORS,SIZES,SHADOWS,FONTS,assets} from '../constants';
+import {CircleButton,RectButton,Subinfo,FocusedStatusBar,DetailsDesc,DetailsBid} from '../components';
+import { useGlobalSearchParams,useLocalSearchParams } from 'expo-router';
 
-//const Stack=createNativeStackNavigator();
-const theme={...DefaultTheme,colors:{
-    ...DefaultTheme.colors,
-    backgroundColor:"transparent"
-}}
-
-const Details = ({data}) => {
-  const [loaded]=useFonts({
-    InterBold:require('../assets/fonts/Inter-Bold.ttf'),
-    InterSemiBold:require('../assets/fonts/Inter-SemiBold.ttf'),
-    InterMedium:require('../assets/fonts/Inter-Medium.ttf'),
-    InterRegular:require('../assets/fonts/Inter-Bold.ttf'),
-})
-const router=useRouter();
-  console.log(data);
-  return (
-    <SafeAreaView theme={theme} style={
-      {flex:1,backGroundColor:'#ffcccc'}
-      }>
-      <Stack.Screen options={{
-          headerShown:false,
-      }}
-      />
-     
-      <ScrollView>
-         <View>
-          <Text>
-            {data}
-          </Text>
-         </View>
-              
-              
-         
-      </ScrollView>
-  </SafeAreaView>
+const Details = () => {
+  const local=useLocalSearchParams();
+  //const {data}=local.data;
  
+    console.log(local.data)
+ 
+
+  return (
+    <View>
+      <Text>details</Text>
+    </View>
   )
 }
 
